a6b2991a350fb1ed10666b1ac0fe6846b1c1df46
AlexandraYazopova <mailto:alexandra.yaz@gmail.com>
2018-08-26 15:56:36 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,19 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate.
+*
+* @author Alexandra Yazopova (mailto:alexandra.yaz@gmail.com)
+*/
 public class Calculate {
-	
+		
+		/**
+		* Main.
+		* @param args - args.
+		*/
 		public static void main (String[] args) {
 			System.out.println ("Hello World");
 		}
 
 }
+
