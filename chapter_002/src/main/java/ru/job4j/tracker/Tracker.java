ecd8e83577b195b44df48cb6b959fae0336cdbc7
AlexandraYazopova <mailto:alexandra.yaz@gmail.com>
2018-12-08 18:41:58 +0300

    3. Изменить программу Tracker из 2-го модуля [#10039]. Доработала методы replace, delete в классе Tracker.

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,9 +59,9 @@ public class Tracker {
      */
     public boolean replace(String id, Item item) {
         boolean result = false;
-        for (Item i: items) {
-            if (i.getId().equals(id)) {
-                items.set(items.indexOf(i), item);
+        for (int i = 0; i != items.size(); i++) {
+            if (items.get(i).getId().equals(id)) {
+                items.set(i, item);
                 result = true;
                 break;
             }
@@ -75,8 +75,8 @@ public class Tracker {
      */
     public boolean delete(String id) {
         boolean result = false;
-        for (Item i: items) {
-            if (i.getId().equals(id)) {
+        for (int i = 0; i != items.size(); i++) {
+            if (items.get(i).getId().equals(id)) {
                 items.remove(i);
                 result = true;
                 break;
